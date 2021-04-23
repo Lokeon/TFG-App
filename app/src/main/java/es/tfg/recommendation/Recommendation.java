92662d8dc6587b57e8228ee7d25ea252285c0c0d
Lokeon <kevin.lopezcala99@gmail.com>
2021-04-23 17:43:45 +0200

    :penguin: fixed Recommendation

--- app/src/main/java/es/tfg/recommendation/Recommendation.java
+++ app/src/main/java/es/tfg/recommendation/Recommendation.java
@@ -21,7 +21,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
@@ -155,6 +158,19 @@ public class Recommendation extends AppCompatActivity {
         }
     }
 
+    protected String getErrorFromServer(InputStream error) throws IOException {
+
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(error))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                builder.append(line);
+            }
+        }
+
+        return builder.toString();
+    }
+
     class GetUsername extends AsyncTask<UserInfo, Void, String> {
 
         @Override
@@ -221,7 +237,8 @@ public class Recommendation extends AppCompatActivity {
         private String gname2;
         private String id3;
         private String gname3;
-
+        private TextView TxtReco;
+        private HttpURLConnection urlConnection = null;
 
         @Override
         protected void onPreExecute() {
@@ -243,6 +260,7 @@ public class Recommendation extends AppCompatActivity {
             game3.setVisibility(View.INVISIBLE);
 
             progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+            TxtReco = (TextView) findViewById(R.id.TxtReco);
 
             progressBar.setVisibility(View.VISIBLE);
         }
@@ -250,7 +268,6 @@ public class Recommendation extends AppCompatActivity {
         @Override
         protected String doInBackground(UserInfo... strings) {
             String text;
-            HttpURLConnection urlConnection = null;
 
             try {
                 URL url = new URL(getResources().getString(R.string.ip_recommendation));
@@ -264,6 +281,7 @@ public class Recommendation extends AppCompatActivity {
                 urlConnection.connect();
 
                 InputStream inputStream = urlConnection.getInputStream();
+
                 text = new Scanner(inputStream).useDelimiter("\\A").next();
 
             } catch (Exception e) {
@@ -281,61 +299,68 @@ public class Recommendation extends AppCompatActivity {
 
             if (results != null) {
                 try {
+                    if (urlConnection.getResponseCode() == 402) {
+                        progressBar.setVisibility(View.GONE);
+                        TxtReco.setVisibility(View.VISIBLE);
+                        TxtReco.setText(R.string.recomen);
+                    } else {
+                        JSONObject jsonobject = new JSONObject(results);
 
-                    JSONObject jsonobject = new JSONObject(results);
+                        progressBar.setVisibility(View.GONE);
+                        TxtReco.setVisibility(View.GONE);
 
-                    progressBar.setVisibility(View.GONE);
+                        game1.setVisibility(View.VISIBLE);
+                        name1.setText(jsonobject.getString("nameGame1"));
+                        gname1 = jsonobject.getString("nameGame1");
+                        id1 = jsonobject.getString("idGame1");
+                        byte[] decodedString = Base64.decode(jsonobject.getString("image1"), Base64.DEFAULT);
+                        image1.setImageBitmap(BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length));
 
-                    game1.setVisibility(View.VISIBLE);
-                    name1.setText(jsonobject.getString("nameGame1"));
-                    gname1 = jsonobject.getString("nameGame1");
-                    id1 = jsonobject.getString("idGame1");
-                    byte[] decodedString = Base64.decode(jsonobject.getString("image1"), Base64.DEFAULT);
-                    image1.setImageBitmap(BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length));
-
-                    game2.setVisibility(View.VISIBLE);
-                    name2.setText(jsonobject.getString("nameGame2"));
-                    gname2 = jsonobject.getString("nameGame2");
-                    id2 = jsonobject.getString("idGame2");
-                    byte[] decodedString2 = Base64.decode(jsonobject.getString("image2"), Base64.DEFAULT);
-                    image2.setImageBitmap(BitmapFactory.decodeByteArray(decodedString2, 0, decodedString2.length));
+                        game2.setVisibility(View.VISIBLE);
+                        name2.setText(jsonobject.getString("nameGame2"));
+                        gname2 = jsonobject.getString("nameGame2");
+                        id2 = jsonobject.getString("idGame2");
+                        byte[] decodedString2 = Base64.decode(jsonobject.getString("image2"), Base64.DEFAULT);
+                        image2.setImageBitmap(BitmapFactory.decodeByteArray(decodedString2, 0, decodedString2.length));
 
 
-                    game3.setVisibility(View.VISIBLE);
-                    name3.setText(jsonobject.getString("nameGame3"));
-                    gname3 = jsonobject.getString("nameGame3");
-                    id3 = jsonobject.getString("idGame3");
-                    byte[] decodedString3 = Base64.decode(jsonobject.getString("image3"), Base64.DEFAULT);
-                    image3.setImageBitmap(BitmapFactory.decodeByteArray(decodedString3, 0, decodedString3.length));
+                        game3.setVisibility(View.VISIBLE);
+                        name3.setText(jsonobject.getString("nameGame3"));
+                        gname3 = jsonobject.getString("nameGame3");
+                        id3 = jsonobject.getString("idGame3");
+                        byte[] decodedString3 = Base64.decode(jsonobject.getString("image3"), Base64.DEFAULT);
+                        image3.setImageBitmap(BitmapFactory.decodeByteArray(decodedString3, 0, decodedString3.length));
 
-                    image1.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            bundleSend.putString("name_game", gname1);
-                            bundleSend.putString("id_game", id1);
-                            startActivity(new Intent(Recommendation.this, GameViewUser.class).putExtras(bundleSend));
-                        }
-                    });
+                        image1.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                bundleSend.putString("name_game", gname1);
+                                bundleSend.putString("id_game", id1);
+                                startActivity(new Intent(Recommendation.this, GameViewUser.class).putExtras(bundleSend));
+                            }
+                        });
 
-                    image2.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            bundleSend.putString("name_game", gname2);
-                            bundleSend.putString("id_game", id2);
-                            startActivity(new Intent(Recommendation.this, GameViewUser.class).putExtras(bundleSend));
-                        }
-                    });
+                        image2.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                bundleSend.putString("name_game", gname2);
+                                bundleSend.putString("id_game", id2);
+                                startActivity(new Intent(Recommendation.this, GameViewUser.class).putExtras(bundleSend));
+                            }
+                        });
 
-                    image3.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            bundleSend.putString("name_game", gname3);
-                            bundleSend.putString("id_game", id3);
-                            startActivity(new Intent(Recommendation.this, GameViewUser.class).putExtras(bundleSend));
-                        }
-                    });
+                        image3.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                bundleSend.putString("name_game", gname3);
+                                bundleSend.putString("id_game", id3);
+                                startActivity(new Intent(Recommendation.this, GameViewUser.class).putExtras(bundleSend));
+                            }
+                        });
+                    }
 
-                } catch (JSONException e) {
+
+                } catch (JSONException | IOException e) {
                     e.printStackTrace();
                 }
             }
